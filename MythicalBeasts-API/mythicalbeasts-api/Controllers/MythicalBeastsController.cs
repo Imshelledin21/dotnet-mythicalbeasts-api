ad4ad87f86bf57802bb7d7709e24f9bcf2a3dbec
Imshelledin21 <5133739+Imshelledin21@users.noreply.github.com>
2025-04-30 09:46:10 -0500

    Updated Api

--- MythicalBeasts-API/mythicalbeasts-api/Controllers/MythicalBeastsController.cs
+++ MythicalBeasts-API/mythicalbeasts-api/Controllers/MythicalBeastsController.cs
@@ -1,25 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using mythicalbeasts_api.Models;
+using mythicalbeasts_api.Utils;
 using System.Diagnostics;
 
 namespace mythicalbeasts_api.Controllers
 {
+
     [ApiController]
     [Route("[controller]")]
     public class MythicalBeastsController : ControllerBase
     {
-        [HttpGet(Name = "GetMythicalBeasts")]
-        public MythicalBeasts Get()
+        private readonly ILogger<MythicalBeastsController> _logger;
+
+        public MythicalBeastsController(ILogger<MythicalBeastsController> logger)
         {
-            Console.WriteLine(Activity.Current.Id);
-            return new MythicalBeasts()
-            {
-                Name = "George",
-                Type = "Centaur",
-                Description = "A stalwart defender of butterflies and chess pieces.",
-                Job = "Cooper",
-                Id = 1
-            };
+            _logger = logger;
+        }
+        
+
+        [HttpGet("/MythicalBeasts", Name = "GetMythicalBeasts")]
+        public async Task<List<MythicalBeasts>> Get()
+        {
+            _logger.LogInformation("New Request to get all Mythical Beasts.");
+
+            var http = new Utils.Http();
+
+            var beasts = await http.Get(null);
+            return beasts;
         }
     }
 }
---
+++ MythicalBeasts-API/mythicalbeasts-api/Utils/Http.cs
@@ -0,0 +1,22 @@
+﻿using mythicalbeasts_api.Models;
+
+namespace mythicalbeasts_api.Utils
+{
+    public class Http
+    {
+        private readonly ILogger<Http> _logger;
+
+
+        public async Task<List<MythicalBeasts>> Get(int? id)
+        {
+            string baseUrl = Environment.GetEnvironmentVariable("BASE_DATA_API_ENDPOINT");
+            string fullUrl = baseUrl + "/MythicalBeasts/" + id;
+
+
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetFromJsonAsync<List<MythicalBeasts>>(fullUrl);
+
+            return response;
+        }
+    }
+}
